4634b208fe839251715ff6e76ab7d65a56e5cd88
joelberhanee <jobx24@student.bth.se>
2025-05-14 14:12:37 +0200

    Kmom05 alla mappar redo

--- src/Controller/ApiControllerJson.php
+++ src/Controller/ApiControllerJson.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -182,5 +185,42 @@ class ApiControllerJson extends AbstractController
         ];
 
         return new JsonResponse($response);
+    }
+
+    #[Route('/api/library/books', name: 'api_library_books', methods: ['GET'])]
+    public function getAllBooks(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $books = $entityManager->getRepository(Book::class)->findAll();
+
+        $data = array_map(function (Book $book) {
+            return [
+                'id' => $book->getId(),
+                'title' => $book->getTitle(),
+                'isbn' => $book->getIsbn(),
+                'author' => $book->getAuthor(),
+                'image' => $book->getImage(),
+                ];
+        }, $books);
+
+        return new JsonResponse($data);
+    }
+    #[Route('/api/library/book/{isbn}', name: 'api_library_book_by_isbn', methods: ['GET'])]
+    public function getBookByIsbn(string $isbn, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $book = $entityManager->getRepository(Book::class)->findOneBy(['isbn' => $isbn]);
+
+        if (!$book) {
+            return new JsonResponse(['error' => 'Bok hittades inte.'], 404);
+        }
+
+        $data = [
+            'id' => $book->getId(),
+            'title' => $book->getTitle(),
+            'isbn' => $book->getIsbn(),
+            'author' => $book->getAuthor(),
+            'image' => $book->getImage(),
+        ];
+
+        return new JsonResponse($data);
     }
 }
