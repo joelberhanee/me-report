cfdc3efa87dfc7a2e064056b93f0551f695deb9a
joelberhanee <jobx24@student.bth.se>
2025-05-17 18:52:33 +0200

    Kmom06 Card/CardHand uppdaterad

--- src/Card/CardHand.php
+++ src/Card/CardHand.php
@@ -31,34 +31,31 @@ class CardHand
         }
         return trim($output);
     }
+
     public function getSum(): int
     {
         $sum = 0;
-        $aces = 0;
-    
+        $numAces = 0;
+
         foreach ($this->cards as $card) {
             $value = $card->getValue();
-    
-            switch ($value) {
-                case 'J':
-                case 'Q':
-                case 'K':
-                    $sum += 10;
-                    break;
-                case 'A':
-                    $aces++;
-                    $sum += 1;
-                    break;
-                default:
-                    $sum += (int)$value;
+
+            if (is_numeric($value)) {
+                $sum += (int)$value;
+            } elseif (in_array($value, ['J', 'Q', 'K'])) {
+                $sum += 10;
+            } elseif ($value === 'A') {
+                $numAces++;
+                $sum += 1;
             }
         }
-    
-        while ($aces > 0 && $sum + 10 <= 21) {
-            $sum += 10;
-            $aces--;
+
+        for ($i = 0; $i < $numAces; $i++) {
+            if ($sum + 10 <= 21) {
+                $sum += 10;
+            }
         }
-    
+
         return $sum;
-    }    
+    }
 }
